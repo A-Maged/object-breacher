e950514a37731426f5dcea185d7c14b69cade40b
maged dev <maged.dev@gmail.com>
2021-01-12 17:27:24 +0200

    added more use cases to benchmark

--- bench.js
+++ bench.js
@@ -9,10 +9,17 @@ const _ = require("lodash");
 
 const newObj = () => ({
   a: {
+    x: null,
     b: ["c", "d"],
     e: "f",
     g: {
-      k: "l",
+      h: {
+        get: {
+          the: {
+            prize: "got it",
+          },
+        },
+      },
     },
     "dot.dot": "don't trip now",
   },
@@ -24,26 +31,32 @@ suite1
   .add("lodash#get", function () {
     let obj = newObj();
     _.get(obj, "a.b");
+    _.get(obj, ["a", "g", "h", "get", "the", "prize"]);
   })
   .add("object-breacher#get", function () {
     let obj = newObj();
     objectBreacher.get(obj, "a.b");
+    objectBreacher.get(obj, ["a", "g", "h", "get", "the", "prize"]);
   })
   .add("hoek#get", function () {
     let obj = newObj();
     hoek.reach(obj, "a.b");
+    hoek.reach(obj, ["a", "g", "h", "get", "the", "prize"]);
   })
   .add("nested-property#get", function () {
     let obj = newObj();
     nestedProperty.get(obj, "a.b");
+    nestedProperty.get(obj, ["a", "g", "h", "get", "the", "prize"]);
   })
   .add("dot-prop#get", function () {
     let obj = newObj();
     dotProp.get(obj, "a.b");
+    dotProp.get(obj, ["a", "g", "h", "get", "the", "prize"]);
   })
   .add("object-path#get", function () {
     let obj = newObj();
     objectPath.get(obj, "a.b");
+    objectPath.get(obj, ["a", "g", "h", "get", "the", "prize"]);
   })
   .on("start", onStart)
   .on("cycle", onCycle)
@@ -56,22 +69,27 @@ suite2
   .add("lodash#set", function () {
     let obj = newObj();
     _.set(obj, "a.b.z", ";)");
+    _.set(obj, ["a", "g", "h", "get", "the", "prize"], "new prize");
   })
   .add("nested-property#set", function () {
     let obj = newObj();
     nestedProperty.set(obj, "a.b.z", ";)");
+    nestedProperty.set(obj, ["a", "g", "h", "get", "the", "prize"], "new prize");
   })
   .add("dot-prop#set", function () {
     let obj = newObj();
     dotProp.set(obj, "a.b.z", ";)");
+    dotProp.set(obj, ["a", "g", "h", "get", "the", "prize"], "new prize");
   })
   .add("object-breacher#set", function () {
     let obj = newObj();
     objectBreacher.set(obj, "a.b.z", ";)");
+    objectBreacher.set(obj, ["a", "g", "h", "get", "the", "prize"], "new prize");
   })
   .add("object-path#set", function () {
     let obj = newObj();
     objectPath.set(obj, "a.b.z", ";)");
+    objectPath.set(obj, ["a", "g", "h", "get", "the", "prize"], "new prize");
   })
   .on("cycle", onCycle)
   .on("complete", onComplete)
@@ -86,7 +104,5 @@ function onCycle(event) {
 }
 
 function onComplete() {
-  console.log(
-    `\nFastest is  ${this.filter("fastest").map("name")} \n`.brightGreen
-  );
+  console.log(`\nFastest is  ${this.filter("fastest").map("name")} \n`.brightGreen);
 }
