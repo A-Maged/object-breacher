2f96f5816a536e29d156fb4f1d6a4b784eadac17
maged dev <maged.dev@gmail.com>
2021-01-29 00:38:57 +0200

    optimized set

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,7 @@
-export const PATH_SEPERATOR = ".";
-
-export function formatKeys(
-  path: string | Array<string>,
-  pathSeparator: string | undefined
-) {
-  if (typeof path !== "string" && !Array.isArray(path))
-    throw Error("Path must be of type String or Array");
-
-  return Array.isArray(path) ? path : path.split(pathSeparator || PATH_SEPERATOR);
-}
+export const PATH_SEPERATOR = '.';
 
 export function isObject(value: any) {
-  return value && typeof value === "object" && value.constructor === Object;
+  return (
+    value && typeof value === 'object' && value.constructor === Object
+  );
 }
